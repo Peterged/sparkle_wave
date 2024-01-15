f00eabba1a844eb33f082703af53bd397159ca46
Guardian <petergedon17@gmail.com>
2024-01-15 22:00:01 +0800

    IC

---
+++ views/users/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>Login</title>
+</head>
+
+<body>
+    <h1>Login</h1>
+    <div class="box">
+        <form action="login_proses.php" method="post">
+            <input type="text" name="username" placeholder="Username" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <input type="submit" class="submit" name="submit" value="LOGIN">
+        </form>
+    </div>
+</body>
+
+</html>
